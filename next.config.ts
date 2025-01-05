b42b1c97ad4ed79d37043b272bb26aa3728ce916
suenyiyang <suenyiyang+github@gmail.com>
2025-01-05 13:17:34 +0800

    feat: add svgr dep

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const withMdx = createMdx({
   }
 })
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
+};
 
 export default withMdx(nextConfig);
