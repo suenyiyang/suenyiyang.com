527226c76f1c02d2e56381cccd2ebabe3a37d9b8
Yiyang Suen <suenyiyang+github@gmail.com>
2025-05-25 16:47:55 +0800

    chore: rollback vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/postcss";
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     reactRouter(),
     { enforce: "pre", ...mdx() },
     react({
@@ -20,6 +20,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
