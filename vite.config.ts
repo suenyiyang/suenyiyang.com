6040f61d437fc0dabe47a547a76abba1cf900084
Yiyang Suen <suenyiyang+github@gmail.com>
2025-05-26 00:26:32 +0800

    feat: add front matter handler

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/postcss";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
+import remarkFrontmatter from "remark-frontmatter";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
     reactRouter(),
-    { enforce: "pre", ...mdx() },
+    { enforce: "pre", ...mdx({ remarkPlugins: [remarkFrontmatter] }) },
     react({
       include: /\.(mdx|js|jsx|ts|tsx)$/,
     }),
