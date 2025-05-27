2b867fa6130af2c0fce9fb9b79579179604db783
Yiyang Suen <suenyiyang+github@gmail.com>
2025-05-27 23:10:00 +0800

    chore: update vite config to fix click events not fire problem

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/postcss";
-import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
+import tailwindcss from "@tailwindcss/vite";
 import remarkFrontmatter from "remark-frontmatter";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     reactRouter(),
     { enforce: "pre", ...mdx({ remarkPlugins: [remarkFrontmatter] }) },
-    react({
-      include: /\.(mdx|js|jsx|ts|tsx)$/,
-    }),
     svgr({
       include: "**/*.svg",
     }),
@@ -21,11 +17,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
