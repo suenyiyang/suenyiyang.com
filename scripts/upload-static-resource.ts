baf5a8e8691b2edd277cba9c9f617fc5dcf72ca0
Yiyang Suen <suenyiyang+github@gmail.com>
2025-11-28 20:49:41 +0800

    chore: add upload after build

---
+++ scripts/upload-static-resource.ts
@@ -0,0 +1,79 @@
+import OSSClient from 'ali-oss';
+import assert from 'node:assert';
+import glob from 'fast-glob';
+import path from 'node:path';
+
+interface OSSConfigItem {
+  region: string;
+  bucket: string;
+}
+
+assert(process.env.BUILD_REGION);
+
+const BUILD_REGION_TO_OSS_CONFIG: Record<string, OSSConfigItem[]> = {
+  cn: [
+    {
+      region: 'oss-cn-chengdu',
+      bucket: 'static-resource-cn-cd',
+    },
+  ],
+  jp: [
+    {
+      region: 'oss-ap-northeast-1',
+      bucket: 'static-resource-jp',
+    },
+  ],
+};
+
+const createOSSClient = (config: OSSConfigItem) => {
+  assert(process.env.OSS_ACCESS_KEY_ID);
+  assert(process.env.OSS_ACCESS_KEY_SECRET);
+
+  const client = new OSSClient({
+    region: config.region,
+    accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+    accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+    bucket: config.bucket,
+  });
+
+  return client;
+};
+
+const getFilePathList = async (folder: string) => {
+  const filePathList = glob(
+    `${folder}/**/*`,
+    {
+      absolute: true,
+      dot: true,
+      ignore: ['**/*.html'],
+    },
+  );
+
+  return filePathList;
+};
+
+const OSS_CONFIG = BUILD_REGION_TO_OSS_CONFIG[process.env.BUILD_REGION];
+
+assert(OSS_CONFIG);
+
+const main = async () => {
+  const outputFolder = path.resolve(__dirname, '../build');
+  const filePathList = await getFilePathList(outputFolder);
+
+  for (const config of OSS_CONFIG) {
+    const client = createOSSClient(config);
+
+    for (const filePath of filePathList) {
+      const relativePath = filePath.replace(outputFolder, '');
+      await client.put(
+        relativePath,
+        filePath,
+      );
+    }
+  }
+};
+
+
+main().catch(error => {
+  console.error('Failed to upload static resource.', error);
+});
