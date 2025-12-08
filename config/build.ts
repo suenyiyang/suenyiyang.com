3703828fb739e870d250792181a09a205f28a339
Yiyang Suen <suenyiyang+github@gmail.com>
2025-12-09 00:09:30 +0800

    chore: migrate from aliyun oss to cloudflare R2 (#2)

--- config/build.ts
+++ config/build.ts
@@ -1,19 +1,3 @@
 import { ENV_BASE } from "./env";
 
-export interface OSSConfigItem {
-  region: string;
-  bucket: string;
-}
-
-export const OSS_CONFIG: OSSConfigItem[] = [
-  {
-    region: 'oss-cn-chengdu',
-    bucket: 'static-resource-cn-cd',
-  },
-  {
-    region: 'oss-ap-northeast-1',
-    bucket: 'static-resource-jp',
-  },
-];
-
 export const BASE = ENV_BASE ? ENV_BASE : '/';
--- config/env.ts
+++ config/env.ts
@@ -2,10 +2,14 @@ export const ENV_BASE = process.env.BASE;
 
 export const ENV_BUILD_REGION = process.env.BUILD_REGION;
 
-export const ENV_OSS_ACCESS_KEY_ID = process.env.OSS_ACCESS_KEY_ID;
+export const ENV_R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID;
 
-export const ENV_OSS_ACCESS_KEY_SECRET = process.env.OSS_ACCESS_KEY_SECRET;
+export const ENV_R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
 
-export const ENV_OSS_PREFIX_PATH = process.env.OSS_PREFIX_PATH;
+export const ENV_R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
+
+export const ENV_R2_BUCKET_NAME = process.env.R2_BUCKET_NAME;
+
+export const ENV_R2_PUBLIC_URL = process.env.R2_PUBLIC_URL;
 
 export const ENV_WALINE_SERVER_URL = process.env.WALINE_SERVER_URL;
