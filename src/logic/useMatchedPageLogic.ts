e9bcff50c01f14e2f14a08516b77f13fb3461db7
Yiyang Suen <suenyiyang+github@gmail.com>
2025-12-09 10:01:56 +0800

    fix: normalize pathname in match page logic

--- src/logic/useMatchedPageLogic.ts
+++ src/logic/useMatchedPageLogic.ts
@@ -6,7 +6,10 @@ export const useMatchedPageLogic = (location: Location) => {
   const matchedPage = useMemo(() => {
     const pathname = location.pathname;
 
-    const match = allPages.find((page) => page._meta.path === pathname);
+    const match = allPages.find((page) => {
+      const normalizedPathname = pathname === '/' ? pathname : pathname.replace(/\/?$/, '');
+      return page._meta.path === normalizedPathname;
+    });
 
     if (!match) {
       return null;
